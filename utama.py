12d9a39a3039dd5d127ba65c4d2ca7cc3da5c896
Hendri Mardani <49816104+hendrimardani@users.noreply.github.com>
2023-03-18 13:52:09 +0700

    Update utama.py

--- utama.py
+++ utama.py
@@ -12,4 +12,4 @@ mengetik("...Hallo, model ini sudah di training oleh author Hendri Mardani...\n\
 ...Ini adalah NLP deteksi kalimat negatif, netral dan positif.\n\
 Contoh Penggunaan:\n\
 1.Jangan dibeli barangnya jelek\n\
-2.Mending beli ditoko sebelah aja daripada disini")\n
+2.Mending beli ditoko sebelah aja daripada disini\n")
