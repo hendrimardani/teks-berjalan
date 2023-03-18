a91f2862ada1455abb99ed5703554a98819c839b
Hendri Mardani <49816104+hendrimardani@users.noreply.github.com>
2023-03-18 13:50:15 +0700

    Update utama.py

--- utama.py
+++ utama.py
@@ -8,4 +8,8 @@ def mengetik(s):
         sys.stdout.flush()
         time.sleep(random.random() * 0.1)
 
-mengetik("...Hallo, Model ini sudah di training oleh author Hendri Mardani...\n...ini adalah NLP deteksi kalimat negatif, netral dan positif.")
+mengetik("...Hallo, model ini sudah di training oleh author Hendri Mardani...\n\
+...Ini adalah NLP deteksi kalimat negatif, netral dan positif.\n\
+Contoh Penggunaan:\n\
+1.Jangan dibeli barangnya jelek\n\
+2.Mending beli ditoko sebelah aja daripada disini")
